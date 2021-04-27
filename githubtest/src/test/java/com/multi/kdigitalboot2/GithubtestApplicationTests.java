0cab51b2269c574ccde1cffb653f5c0d3609b388
JUN <JUN@DESKTOP-7SLETGG>
2021-04-27 10:20:06 +0900

    first commit multi sts class(HelloController add)

---
+++ githubtest/src/test/java/com/multi/kdigitalboot2/GithubtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.kdigitalboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
